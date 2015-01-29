f4e813d00a4833ded706656161976034b3a6bd66
Collin hardy <Collin.Hardy25@gmail.com>
2015-01-29 11:16:45 -0800

    edits

--- controller/create-db.php
+++ controller/create-db.php
@@ -3,5 +3,9 @@
  $connection = new mysqli($host, $unsername, $password);
  
  if ($connection->connect_error) {
-    
+     die("Error: " . $connection->connect_error);
 }
+ else {
+    echo "success: " . $connection->host_info;
+}
+$connection->close();
