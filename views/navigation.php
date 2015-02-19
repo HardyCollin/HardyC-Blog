d5f9cffe3ffb5bdb5246ee3aad18e4243f985319
Collin hardy <Collin.Hardy25@gmail.com>
2015-02-19 11:25:29 -0800

    linked my blog to my portfolio

--- views/navigation.php
+++ views/navigation.php
@@ -9,10 +9,9 @@
 
 <nav>
     <ul>
-        <li><a href="<?php echo $path . "/index.php" ?>">Home</a></li>
+        <li><a href="<?php echo $path . "/index.html" ?>">Home</a></li>
+        <li><a href="<?php echo $path . "/index.php" ?>">Blog Home</a></li>        
         <li><a href="<?php echo $path . "/post.php" ?>">Blog Post Form</a></li>
         <li><a href="<?php echo $path . "/controller/logout-user.php" ?>">Log Out</a></li>
-        <li><a href="<?php echo $path . "/public_html/index.html" ?>">About Me</a></li>
-        <li><a href="<?php echo $path . "/public_html/index_2.html" ?>">snake</a></li>
     </ul>
 </nav>
